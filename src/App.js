5a5d1ce947b309a21aaa06ad8977ae5bab72d9ae
Andy Charles <developedbykenan@gmail.com>
2020-08-25 17:16:30 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   static defaultProps = {
@@ -7,6 +8,7 @@ class App extends Component {
 
   render() {
     const { store } = this.props
+    const onStage = store.participants.filter(participant => participant.onStage);
     return (
       <main className='App'>
         <header className='App-header'>
@@ -16,6 +18,7 @@ class App extends Component {
           {store.participants.map(participant => (
             <Split
               key={participant.id}
+              id={participant.id}
               name={participant.name}
               avatar={participant.avatar}
               session={participant.inSession}
@@ -32,11 +35,20 @@ function Split (props) {
   return <div className='split'>
            <h2>{props.id}</h2>
            <h3>{props.name}</h3>
-           <img src={props.avatar} />
-           <h3>{JSON.stringify(props.inSession)}</h3>
-           <h3>{props.onStage}</h3>
-  </div>
+           <img src={props.avatar} alt='avatar'/>
+           <div>
+           <h3>{'In Session' + JSON.stringify(props.session)}</h3>
+           <h3>{'On Stage' + JSON.stringify(props.stage)}</h3>
+           </div>
+        </div>
+
+        
 }
+
+  // store.participants.filter(part => part.onStage===true)
+  // .map( part => <Split name=(part.name)/> )
+
+
 
 export default App;
 
