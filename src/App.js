b255d9e915dffd93297ba7cc1571ede7d2a45210
Harrison Winkler <harryewinkler@thinkful.com>
2020-08-25 14:39:51 -0400

    beginning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import List from './List';
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <List />
     </main>
   );
 }
